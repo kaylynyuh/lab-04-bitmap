b1f7ea7d77e13ed4d7baabaf2129b44a53a3ea29
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-16 11:56:01 -0700

    got the color array seperated, now we're stuck

--- bitmap-pegastickacorn/index.js
+++ bitmap-pegastickacorn/index.js
@@ -4,11 +4,11 @@ const bitmapFileHelper = require('./lib/bitmap-file-helper.js');
 
 
 bitmapFileHelper.readFile(function(err, bitmap) {
-  console.log(bitmap.changeColor());
-
+  // console.log('hello!');
+  bitmap.changeColor();
   // do transform
   bitmapFileHelper.writeFile(bitmap, function(err, data){
-
+    // console.log(bitmap);
   });
 });
 
--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -8,7 +8,22 @@ const Bitmap = module.exports = function (buf) {
 };
 
 Bitmap.prototype.changeColor = function() {
+  console.log('color array', this.colorArray);
+  console.log('color array length', this.colorArray.length)
+
+  // console.log('whole buffer', this.buffer.toString('hex'));
+  // console.log('colorArray before', this.colorArray.toString('hex'));
   for(var i = 0; i < this.colorArray.length; i++) {
-    console.log(this.colorArray.readIntLE(i, 1));
+    this.colorArray.readIntLE(i, 1);
+    this.colorArray.writeIntLE(0, i, 1);
+    if((i + 3) % 4){
+      this.colorArray.writeIntLE(255, i, 1);
+    }
+    //this copies from the colorArray(source), to the buffer(target) starting at the target[54] and the whole source[0, source.length]
+    // this.colorArray.copy(this.buffer, 54);
+    // console.log('colorArray after', this.colorArray.toString('hex'));
   }
 };
+
+//if we want to invert colors, later, we will probably need to set up an inverted buffer:
+// this.invertedArray.writeIntLE(255 - originalColor, i, 1);
