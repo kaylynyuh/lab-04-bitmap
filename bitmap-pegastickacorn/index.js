d1f88cc318deb8fc033f863b9d5e87cb7b203269
kaylynyuh <yuhkay4@gmail.com>
2016-09-16 09:51:14 -0700

    working on color transforming logic

--- bitmap-pegastickacorn/index.js
+++ bitmap-pegastickacorn/index.js
@@ -4,10 +4,12 @@ const bitmapFileHelper = require('./lib/bitmap-file-helper.js');
 
 
 bitmapFileHelper.readFile(function(err, bitmap) {
-  console.log(bitmap);
+  console.log(bitmap.changeColor());
 
   // do transform
   bitmapFileHelper.writeFile(bitmap, function(err, data){
 
-  })
+  });
 });
+
+// bitmap.changeColor();
--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -4,11 +4,11 @@ const Bitmap = module.exports = function (buf) {
   this.buffer = buf;
   this.size = buf.readUInt32LE(2);
   this.offset = buf.readUInt32LE(10);
-  this.colorPallet = buf.slice(54, this.offset);
+  this.colorArray = buf.slice(54, this.offset);
 };
 
 Bitmap.prototype.changeColor = function() {
-  for(var i = 0; i < buffer.length; i += 4) {
-
-  };
+  for(var i = 0; i < this.colorArray.length; i++) {
+    console.log(this.colorArray.readIntLE(i, 1));
+  }
 };
