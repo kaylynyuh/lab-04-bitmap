ac288be0412a094cec917e1c7c8fa6cfeae9e5b4
kaylynyuh <yuhkay4@gmail.com>
2016-09-18 10:57:08 -0700

    working on grayscale function

--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -3,15 +3,19 @@
 const Bitmap = module.exports = function (buf) {
   this.buffer = buf;
   this.size = buf.readUInt32LE(2);
-  console.log
   this.offset = buf.readUInt32LE(10);
   this.colorArray = buf.slice(this.offset, this.size);
+  // console.log(this.colorArray.length);
 };
 
 Bitmap.prototype.invertColor = function() {
   for(var i = 0; i < this.colorArray.length; i++) {
     this.colorArray[i] = Math.abs(this.colorArray[i] - 255);
   }
+};
 
-
+Bitmap.prototype.grayScaleColor = function () {
+  for(var i = 0; i< this.colorArray.length; i++) {
+    
+  }
 };
