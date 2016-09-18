3cfd5cc0b7956b696ee282346c4721af8a55a879
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-18 10:36:58 -0700

    troubleshooting

--- bitmap-pegastickacorn/index.js
+++ bitmap-pegastickacorn/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const bitmapFileHelper = require('./lib/bitmap-file-helper.js');
+const bitmapFileHelper = require('./lib/bitmap-file-helper');
 
 
 bitmapFileHelper.readFile(function(err, bitmap) {
   // console.log('hello!');
-  bitmap.changeColor();
+  bitmap.changeColor(err, bitmap);
   // do transform
   bitmapFileHelper.writeFile(bitmap, function(err, data){
     // console.log(bitmap);
--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -9,20 +9,25 @@ const Bitmap = module.exports = function (buf) {
 
 Bitmap.prototype.changeColor = function() {
   console.log('color array', this.colorArray);
-  console.log('color array length', this.colorArray.length)
-
+  console.log('color array length', this.colorArray.length);
+  var i = 0;
   // console.log('whole buffer', this.buffer.toString('hex'));
-  // console.log('colorArray before', this.colorArray.toString('hex'));
-  for(var i = 0; i < this.colorArray.length; i++) {
+  console.log('colorArray before', this.colorArray.toString('hex'));
+  if(i < this.colorArray.length) {
+    i++;
+    // console.log('value of i:', i);
     this.colorArray.readIntLE(i, 1);
-    this.colorArray.writeIntLE(0, i, 1);
-    if((i + 3) % 4){
+    // console.log('what we are reading:', this.colorArray.readIntLE(i, 1));
+    this.colorArray.writeIntLE(2, i, 1);
+    // console.log('what we are writing:', this.colorArray.writeIntLE(2, i, 1));
+    if((i + 3)% 4){
       this.colorArray.writeIntLE(255, i, 1);
+      // console.log('what we are overwriting with:', this.colorArray.writeIntLE(255, i, 1));
     }
-    //this copies from the colorArray(source), to the buffer(target) starting at the target[54] and the whole source[0, source.length]
-    // this.colorArray.copy(this.buffer, 54);
-    // console.log('colorArray after', this.colorArray.toString('hex'));
   }
+  //this copies from the colorArray(source), to the buffer(target) starting at the target[54] and the whole source[0, source.length]
+  // this.colorArray.copy(this.buffer, 54);
+  console.log('colorArray after', this.colorArray.toString('hex'));
 };
 
 //if we want to invert colors, later, we will probably need to set up an inverted buffer:
