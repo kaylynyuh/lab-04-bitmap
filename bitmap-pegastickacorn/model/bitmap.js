46e28761fcc2f496a1250550657399b4e1650270
kaylynyuh <yuhkay4@gmail.com>
2016-09-18 19:10:28 -0700

    changes made, grossly vague commit message

--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -17,9 +17,11 @@ Bitmap.prototype.grayScaleColor = function () {
   // for(var i = 0; i< this.colorArray.length; i++) {
   //   this.colorArray[i]
   // }
+  var fillGray = function(gray, array) {
+    this.colorArray = [gray, gray, gray].concat(array.slice(3));
+  };
   this.colorArray = this.colorArray.map(function (newArray) {
-    console.log('HERE IN DA MAPS');
-    var makeGray = (this.colorArray[0] + this.colorArray[1] + this.colorArray[2]) /3;
-    
-  })
+    newArray = (this.colorArray[0] + this.colorArray[1] + this.colorArray[2]) /3;
+    this.colorArray[i] = fillGray(gray,array);
+  });
 };
