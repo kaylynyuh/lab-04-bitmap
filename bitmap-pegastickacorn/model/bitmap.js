872a46a8e426a445bffa19f1976cade139f708ff
kaylynyuh <yuhkay4@gmail.com>
2016-09-18 17:30:33 -0700

    grayscale function not working, still trying to figure it out

--- bitmap-pegastickacorn/model/bitmap.js
+++ bitmap-pegastickacorn/model/bitmap.js
@@ -20,6 +20,6 @@ Bitmap.prototype.grayScaleColor = function () {
   this.colorArray = this.colorArray.map(function (newArray) {
     console.log('HERE IN DA MAPS');
     var makeGray = (this.colorArray[0] + this.colorArray[1] + this.colorArray[2]) /3;
-
+    
   })
 };
